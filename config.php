816c45a256796264200f850387d65714c8db796f
selim song <491497047@qq.com>
2013-11-17 15:34:31 +0800

    'm'

--- config.php
+++ config.php
@@ -123,15 +123,14 @@ function alphaID($in, $to_num = false, $pad_up = false)
   return $out;
 }
 
-function msubstr($str, $start, $len) {
-    $tmpstr = "";
-    $strlen = $start + $len;
-    for($i = 0; $i < $strlen; $i++) {
-        if(ord(substr($str, $i, 1)) > 0xa0) {
-            $tmpstr .= substr($str, $i, 2);
-            $i++;
-        } else
-            $tmpstr .= substr($str, $i, 1);
-    }
-    return $tmpstr;
+function cutstr($string, $length) {
+        preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $string, $info);  
+        for($i=0; $i<count($info[0]); $i++) {
+                $wordscut .= $info[0][$i];
+                $j = ord($info[0][$i]) > 127 ? $j + 2 : $j + 1;
+                if ($j > $length - 3) {
+                        return $wordscut." ...";
+                }
+        }
+        return join('', $info[0]);
 }
